5a4bad355cc7991894b41d19da6d4cdc1de9c58d
Nguyen Tri Man <ntman.ute@gmail.com>
2021-10-24 20:56:23 +0700

    24/10/2021 - 20:56 Add 4_14 Literals.cpp

---
+++ 4_14_litteral.cpp
@@ -0,0 +1,16 @@
+/* The link: https://www.learncpp.com/cpp-tutorial/chars/
+ *
+ * In Cpp: 
+ *          A character can be a single letter, number, symbol, or whitespace.
+ *          
+ *          
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
